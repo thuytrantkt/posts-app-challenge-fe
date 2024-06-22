838fede639917c7fbad897e6293d6b499a069dec
Thuy Tran <christiesunnie@gmail.com>
2024-06-21 23:53:15 -0400

    Change the text color to black

--- src/components/PostDetail/PostDetails.tsx
+++ src/components/PostDetail/PostDetails.tsx
@@ -8,14 +8,16 @@ const PostDetail = ({ post, hasPaddingX = true }: PostDetaiProps) => {
   const { author, content, createdAt, title } = post;
   return (
     <CardContent sx={{ paddingX: hasPaddingX ? 4 : 0 }}>
-      <Typography gutterBottom variant="h5" component="h2">
+      <Typography gutterBottom variant="h5" component="h2" color="black">
         {title}
       </Typography>
-      <Typography paddingBottom={1}>{content}</Typography>
-      <Typography paddingBottom={1}>
+      <Typography paddingBottom={1} color="black">
+        {content}
+      </Typography>
+      <Typography paddingBottom={1} color="black">
         <strong>Author:</strong> {author}
       </Typography>
-      <Typography paddingBottom={1}>
+      <Typography paddingBottom={1} color="black">
         <strong>Posted on:</strong> {dateFormatted(createdAt)}
       </Typography>
     </CardContent>
