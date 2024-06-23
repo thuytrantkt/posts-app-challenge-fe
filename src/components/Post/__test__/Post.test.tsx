bf743e0c921dd39208b9361f585490f903695ccf
Thuy Tran <christiesunnie@gmail.com>
2024-06-23 12:43:26 -0400

    Update test for the Post component

--- src/components/Post/__test__/Post.test.tsx
+++ src/components/Post/__test__/Post.test.tsx
@@ -1,17 +1,83 @@
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import Post from "../Post";
 
-it("renders Post component properly", () => {
-  const postMockData = {
-    author: "John Smith",
-    commentCount: 4,
-    content: "This is my very first post",
-    createdAt: "Thu, 19 Aug 2004 16:21:06 GMT",
-    id: 1,
-    title: "First Post",
-  };
-  const view = render(
-    <Post post={postMockData} handleSelectedPostClick={() => {}} />
-  );
-  expect(view).toMatchSnapshot();
+const postMockData = {
+  author: "John Smith",
+  commentCount: 4,
+  content: "This is my very first post",
+  createdAt: "Thu, 19 Aug 2004 16:21:06 GMT",
+  id: 1,
+  title: "First Post",
+};
+
+describe("Post", () => {
+  it("renders Post component properly", () => {
+    const view = render(
+      <Post post={postMockData} handleSelectedPostClick={() => {}} />
+    );
+    expect(view).toMatchSnapshot();
+  });
+
+  it("should call handleSelectedPostClick when clicked", () => {
+    const handleSelectedPostClick = jest.fn();
+    const handleSelectedPostKeyDown = jest.fn();
+
+    const { container } = render(
+      <Post
+        post={postMockData}
+        handleSelectedPostClick={handleSelectedPostClick}
+        handleSelectedPostKeyDown={handleSelectedPostKeyDown}
+      />
+    );
+
+    const postDiv = container.firstChild;
+
+    if (postDiv) {
+      fireEvent.click(postDiv);
+    }
+
+    expect(handleSelectedPostClick).toHaveBeenCalledTimes(1);
+  });
+
+  it("should call handleSelectedPostKeyDown when a key is pressed as an 'Enter' key", () => {
+    const handleSelectedPostClick = jest.fn();
+    const handleSelectedPostKeyDown = jest.fn();
+
+    const { container } = render(
+      <Post
+        post={postMockData}
+        handleSelectedPostClick={handleSelectedPostClick}
+        handleSelectedPostKeyDown={handleSelectedPostKeyDown}
+      />
+    );
+
+    const postDiv = container.firstChild;
+
+    if (postDiv) {
+      fireEvent.keyDown(postDiv, { key: "Enter" });
+    }
+
+    expect(handleSelectedPostKeyDown).toHaveBeenCalledTimes(1);
+  });
+
+  it("should call handleSelectedPostKeyDown when a key is pressed as a space '' key", () => {
+    const handleSelectedPostClick = jest.fn();
+    const handleSelectedPostKeyDown = jest.fn();
+
+    const { container } = render(
+      <Post
+        post={postMockData}
+        handleSelectedPostClick={handleSelectedPostClick}
+        handleSelectedPostKeyDown={handleSelectedPostKeyDown}
+      />
+    );
+
+    const postDiv = container.firstChild;
+
+    if (postDiv) {
+      fireEvent.keyDown(postDiv, { key: " " });
+    }
+
+    expect(handleSelectedPostKeyDown).toHaveBeenCalledTimes(1);
+  });
 });
